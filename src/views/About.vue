72f49821f24b2c59cd222e12e5ad4dad182b4d34
Dopamine <zzhou998@163.com>
2022-01-04 00:02:46 +0800

    feat: test code

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<script setup>
+  import { useRoute } from 'vue-router';
+  const pageTitle = useRoute().meta.title;
+</script>
+<template>
+  <div>{{ pageTitle }}</div>
+  <div>this is about page</div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<script setup>
+  import { computed } from 'vue';
+  import { useStore } from 'vuex';
+  import { useRoute } from 'vue-router';
+
+  const store = useStore();
+
+  const pageTitle = useRoute().meta.title;
+
+  const count = computed(() => store.state.count);
+  const twoMultipleCount = computed(() => store.getters.twoMultipleCount);
+  const twoMultipleCountAddOne = computed(() => store.getters.twoMultipleCountAddOne);
+  const twoMultipleCountAddN = computed(() => store.getters.twoMultipleCountAddN(4));
+
+  const handleAdd = () => {
+    store.commit('increment', 1);
+  };
+
+  const handleSyncAdd = () => {
+    store.dispatch('asyncIncrement', 2);
+  };
+</script>
+<template>
+  <div>{{ pageTitle }}</div>
+  <div class="title"> This is playground!</div>
+  <button @click="handleAdd">ADD 1</button>
+  <button @click="handleSyncAdd">AsyncADD 2</button>
+  <div>{{ count }}</div>
+  <div>这是 2 倍数：{{ twoMultipleCount }}</div>
+  <div>这是 2 倍数 + 1：{{ twoMultipleCountAddOne }}</div>
+  <div>这是 2 倍数 + n：{{ twoMultipleCountAddN }}</div>
+</template>
+<style lang="less" scoped>
+  .title {
+    padding-bottom: 12px;
+    border-bottom: 1px solid #eee;
+    font-size: 24px;
+    text-align: center;
+  }
+</style>
