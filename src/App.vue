4d7e6b2fbfc85376051ed38187222a713389a1e1
清禾 <zzhou998@163.com>
2022-01-13 16:33:19 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-  import dayjs from 'dayjs';
-  import 'dayjs/locale/zh-cn';
-  import locale from 'ant-design-vue/es/date-picker/locale/zh_CN';
-
-  dayjs.locale('zh-cn');
+  import zhCN from 'ant-design-vue/es/locale/zh_CN';
 </script>
 <template>
-  <a-config-provider :locale="locale">
+  <a-config-provider :locale="zhCN">
     <div class="nav">
       <router-link to="/">首页</router-link>
       <router-link to="/about">关于</router-link>
