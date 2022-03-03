8243006c13f7e1d24257e6df821a0d4ce4f8bbba
dopamine <zzhou998@163.com>
2022-03-03 20:46:33 +0800

    refactor: code

--- components.d.ts
+++ components.d.ts
@@ -4,15 +4,15 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    AButton: typeof import('ant-design-vue/es')['Button'];
-    ACol: typeof import('ant-design-vue/es')['Col'];
-    AConfigProvider: typeof import('ant-design-vue/es')['ConfigProvider'];
-    ADatePicker: typeof import('ant-design-vue/es')['DatePicker'];
-    AInput: typeof import('ant-design-vue/es')['Input'];
-    ARow: typeof import('ant-design-vue/es')['Row'];
-    ASelect: typeof import('ant-design-vue/es')['Select'];
-    ATable: typeof import('ant-design-vue/es')['Table'];
+    AButton: typeof import('ant-design-vue/es')['Button']
+    ACol: typeof import('ant-design-vue/es')['Col']
+    AConfigProvider: typeof import('ant-design-vue/es')['ConfigProvider']
+    ADatePicker: typeof import('ant-design-vue/es')['DatePicker']
+    AInput: typeof import('ant-design-vue/es')['Input']
+    ARow: typeof import('ant-design-vue/es')['Row']
+    ASelect: typeof import('ant-design-vue/es')['Select']
+    ATable: typeof import('ant-design-vue/es')['Table']
   }
 }
 
-export {};
+export { }
