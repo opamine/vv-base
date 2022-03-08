b9b231ae87fb4b2dad842ad0d321b4ad65c90565
dopamine <zzhou998@163.com>
2022-03-08 21:26:43 +0800

    refactor: comments

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,12 +9,10 @@ const instance = axios.create({
 // 添加请求拦截器
 instance.interceptors.request.use(
   (config) => {
-    // 在发送请求之前做些什么
     // config.headers['Authorization'] = 'Bear 0GCHq2l4wLYgaqGQevC0g71KgMtw2xaH';
     return config;
   },
   (error) => {
-    // 对请求错误做些什么
     notification.error({
       message: '提示',
       description: error.message,
@@ -27,12 +25,10 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   (response) => {
     // 2xx 范围内的状态码都会触发该函数。
-    // 对响应数据做点什么
     return response;
   },
   (error) => {
     // 超出 2xx 范围的状态码都会触发该函数。
-    // 对响应错误做点什么
     notification.error({
       message: '提示',
       description: error.message,
